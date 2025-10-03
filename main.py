9d8548fdf75ac52f2919d947118288234ea411d5
jesusweb3 <kirill.gurko.067@gmail.com>
2025-10-04 00:04:32 +0300

    очистка экрана cmd при старте программы

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 # main.py
 
+import os
+import sys
 from src.utils.logger import get_logger
 from src.server.app import start_server_sync
 
 if __name__ == "__main__":
+    if sys.platform == 'win32':
+        os.system('cls')
+    else:
+        os.system('clear')
+
     logger = get_logger(__name__)
     logger.info("Trading Bot запущен")
 
